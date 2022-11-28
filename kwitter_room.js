3c2242198256ae288ef1ee62722a818e0cdc5967
RuveerKapurYT <106600912+RuveerKapurYT@users.noreply.github.com>
2022-11-28 16:41:05 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBX5_1-cBP_ZZ_An_wpXssegfm1cqLZoJg",
+    authDomain: "kwitter-a91e8.firebaseapp.com",
+    projectId: "kwitter-a91e8",
+    storageBucket: "kwitter-a91e8.appspot.com",
+    messagingSenderId: "797333851226",
+    appId: "1:797333851226:web:17cbabc71dc9f6342a5c63"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
